261e5ac639625000326aa656969efe673e21be46
winmin <shannanshuibe_i@163.com>
2017-12-06 02:10:11 +0800

    Modify rot function details

--- pigcrypto/rot.py
+++ pigcrypto/rot.py
@@ -16,10 +16,14 @@ def rotate_letter(letter,n):
         start = ord('a')
     else:
         return letterpyt
-
-    c = ord(letter)-start
-    i = (c+n)%26+start
-    return chr(i)
+    
+    c = ord(letter)
+    if c == 32:
+        return chr(int(c))
+    else:
+        c = ord(letter)-start
+        i = (c+n)%26+start
+        return chr(i)
 
 
 def rot(word,n):
@@ -51,9 +55,9 @@ def rot_loop(word):
 
 
 def test():
-    print (rot('flapp ypig',13))
-    print (rot('synccalcvt',13))
-    print (rot_loop('synccalcvt'))
+    print (rot('flappypig is cool',13))
+    print (rot('syncclcvt vf pbby',13))
+    print (rot_loop('syncclcvt vf pbby'))
 
 
 if __name__ == '__main__':
