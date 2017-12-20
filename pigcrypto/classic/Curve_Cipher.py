e4116206e930a4fe1501022959f5d8fa2e92a8e4
winmin <shannanshuibe_i@163.com>
2017-12-20 21:47:20 +0800

    add rsa extraction

--- pigcrypto/classic/Curve_Cipher.py
+++ pigcrypto/classic/Curve_Cipher.py
@@ -8,7 +8,7 @@
 """
 
 
-def curve_cipher_de(message,x,y):
+def set_key(message,x,y):
     """
     :param message:  The string to be encrypted
 
@@ -17,6 +17,45 @@ def curve_cipher_de(message,x,y):
 
     :return:
     """
-    message = 'The quick brown fox jumps over the lazy dog'
+    #message = 'The quick brown fox jumps over the lazy dog'
 
-    set_key 
+    global e
+    e = []
+    a = list(message.replace(' ',''))
+    for i in range(1,y+1):
+        m = x*(i-1)
+        n = x*i
+        b = a[m:n]
+        e.append(b)
+    return e
+
+
+
+
+
+
+def curve_cipher_en(message,x,y):
+    set_key(message,x,y)
+    new_str = ''
+
+    #set_flag = 0
+    if x % 2 ==0:
+        set_flag = 0
+    else:
+        set_flag =1
+
+
+    for i in reversed(range(y)):
+        for j in reversed(range(x)):
+            new_str += e[i][j]
+            break 
+    print(new_str)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    curve_cipher_en('The quick brown fox jumps over the lazy dog',7,5)
